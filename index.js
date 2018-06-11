08044b2372933df2e8ffc6821f15d2f759856f42
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-06-11 12:55:14 +0200

    Log your own account address

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-addr = {}
+addr = {};
 // All on Ropsten
-addr.erik = '0xbD2940e549C38Cc6b201767a0238c2C07820Ef35'
-addr.patrik = '0xbcEf85708670FA0127C484Ac649724B8028Ea08b'
-addr.jacob = '0xBF9e8395854cE02abB454d5131F45F2bFFB54017'
+addr.erik = "0xbD2940e549C38Cc6b201767a0238c2C07820Ef35";
+addr.patrik = "0xbcEf85708670FA0127C484Ac649724B8028Ea08b";
+addr.jacob = "0xBF9e8395854cE02abB454d5131F45F2bFFB54017";
 
 // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-if (typeof web3 !== 'undefined') {
+if (typeof web3 !== "undefined") {
   // Use Mist/MetaMask's provider
-  console.log('Using metamask')
+  console.log("Using metamask");
   web3js = new Web3(web3.currentProvider);
 } else {
-  console.log('No web3? You should consider trying MetaMask!')
+  console.log("No web3? You should consider trying MetaMask!");
   // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
   web3js = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
+web3.version.getNetwork((err, netId) => {
+  switch (netId) {
+    case "1":
+      console.log("This is mainnet");
+      break;
+    case "2":
+      console.log("This is the deprecated Morden test network.");
+      break;
+    case "3":
+      console.log("This is the ropsten test network.");
+      break;
+    case "4":
+      console.log("This is the Rinkeby test network.");
+      break;
+    case "42":
+      console.log("This is the Kovan test network.");
+      break;
+    default:
+      console.log("This is an unknown network.");
+  }
+});
+
 // Now you can start your app & access web3 freely:
 
+console.log("Running the app! wooo!!");
 
+web3.eth.getAccounts(function(err, accounts) {
+  console.log('My account:', accounts[0]);
+});
